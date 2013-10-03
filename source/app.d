c5e88eb3a623cf37ab50a8b136066164beef0df4
OlaOst <olaa81@gmail.com>
2013-10-03 22:13:18 +0200

    Set up OpenGL context and basic event loop

--- source/app.d
+++ source/app.d
@@ -8,6 +8,43 @@ import derelict.sdl2.sdl;
 
 void main()
 {
+  setupDisplay();
+  
+  bool keepRunning = true;
+  SDL_Event event;
+  
+  while (keepRunning)
+  {
+    while (SDL_PollEvent(&event))
+    {
+      switch (event.type)
+      {
+        case SDL_QUIT:
+          keepRunning = false;
+          break;
+          
+        case SDL_KEYUP:
+          switch (event.key.keysym.sym)
+          {
+            case SDLK_ESCAPE:
+              keepRunning = false;
+              break;
+            
+            default:
+              break;
+          }
+          break;
+          
+        default:
+          break;
+      }
+    }
+  }
+}
+
+
+void setupDisplay()
+{
   DerelictSDL2.load();
   DerelictGL3.load();
   
@@ -21,7 +58,15 @@ void main()
   int screenWidth = 800;
   int screenHeight = 600;
   
-  SDL_CreateWindow("greenfield", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+  auto window = SDL_CreateWindow("greenfield", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+  enforce(window !is null, "Error creating window");
   
-	//writeln("Edit source/app.d to start your project.");
+  auto context = SDL_GL_CreateContext(window);
+  enforce(context !is null, "Error creating OpenGL context");
+  
+  SDL_GL_SetSwapInterval(1);
+  
+  // setup gl viewport and etc
+  
+  DerelictGL3.reload();
 }
