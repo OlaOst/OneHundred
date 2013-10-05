ea7e8cb2ef1dc9247d782cbf2b6ea834b4eb1ce4
OlaOst <olaa81@gmail.com>
2013-10-05 12:54:06 +0200

    Change shader version to 330 core

--- source/renderer.d
+++ source/renderer.d
@@ -11,26 +11,32 @@ class Renderer
 {
   this()
   {
-    auto vertices = [-0.3, -0.3, 
+    /*auto vertices = [-0.3, -0.3, 
                       0.3, -0.3, 
                      -0.3,  0.3, 
-                      0.3,  0.3];
-    auto indices = [0, 1, 2, 3];
+                      0.3,  0.3];*/
+    auto vertices = [-1.0, -1.0,
+                      1.0, -1.0,
+                      0.0,  1.0];
+    //auto indices = [0, 1, 2, 3];
     
     static immutable string shaderSource = `
-      #version 120
+      #version 330 core
       
       vertex:
-        attribute vec2 position;
+        //attribute vec2 position;
+        layout(location = 0) in vec2 position;
         void main(void)
         {
           gl_Position = vec4(position, 0, 1);
         }
       
       fragment:
+        out vec3 color;
         void main(void)
         {
-          gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+          //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+          color = vec3(1, 0, 0);
         }
     `;
   
@@ -38,18 +44,21 @@ class Renderer
     vao.bind();
     
     vbo = new Buffer(vertices);
-    ibo = new ElementBuffer(indices);
+    //ibo = new ElementBuffer(indices);
     
-    shader = new Shader("test", shaderSource);
+    shader = new Shader("shader", shaderSource);
     shader.bind();
     position = shader.get_attrib_location("position");
+    
+    import std.stdio;
+    writeln("position from shader is " ~ position.to!string);
   }
   
   
   public void close()
   {
     shader.remove();
-    ibo.remove();
+    //ibo.remove();
     vbo.remove();
     vao.remove();
   }
@@ -60,14 +69,18 @@ class Renderer
     glClearColor(0.0, 0.0, 0.33, 1.0);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+    shader.bind();
+    
     vbo.bind();
     glEnableVertexAttribArray(position);
     
     glVertexAttribPointer(position, 2, GL_FLOAT, GL_FALSE, 0, null);
     
-    ibo.bind();
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     
-    glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, null);
+    //ibo.bind();
+    
+    //glDrawElements(GL_TRIANGLE_STRIP, 4, GL_UNSIGNED_SHORT, null);
     
     glDisableVertexAttribArray(position);
   }
@@ -78,5 +91,5 @@ private:
   Buffer vbo;
   ElementBuffer ibo;
   Shader shader;
-  GLint position;
+  GLint position = 0;
 }
