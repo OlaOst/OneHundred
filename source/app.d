90c1a1b4a0ce65280d42e99e3f07148bae275590
OlaOst <olaa81@gmail.com>
2014-01-12 20:11:05 +0100

    Refactor app to 100 line limit

--- source/app.d
+++ source/app.d
@@ -1,10 +1,8 @@
 import std.algorithm;
 import std.exception;
-import std.conv;
 import std.math;
 import std.random;
 import std.range;
-import std.stdio;
 
 import artemisd.all;
 import derelict.sdl2.sdl;
@@ -24,6 +22,7 @@ import system.movement;
 import system.renderer;
 import system.physics;
 
+import entityfactory;
 import input;
 import window;
 
@@ -40,20 +39,13 @@ void main()
   world.setSystem(renderer);
   world.initialize();
   
-  auto playerManager = new PlayerManager();
-  
-  Entity playerEntity = world.createEntity();
-  playerEntity.addComponent(new Position(vec2(0.0, 0.0), 0.0));
-  playerEntity.addComponent(new Velocity(vec2(0.0, 0.0), 0.0));
-  playerEntity.addComponent(new Size(0.3));
-  playerEntity.addComponent(new Mass(0.3 ^^ 2));
-  playerEntity.addComponent(new Drawable(0.3, 3, vec3(0.0, 1.0, 0.0)));
-  playerEntity.addComponent(new Input());
-  playerManager.setPlayer(playerEntity, "player");
+  auto playerEntity = createPlayer(world);
   
   playerEntity.addToWorld();
   
   Entity[] entities;
+  
+  entities ~= playerEntity;
   
   auto elements = 100;
   foreach (float index; iota(0, elements))
@@ -61,29 +53,18 @@ void main()
     auto angle = (index/elements) * PI * 2.0;
     auto size = uniform(0.01, 0.1);
     
-    Entity entity = world.createEntity();
-    
-    entity.addComponent(new Position(vec2(cos(angle*5) * (0.3+angle.sqrt), 
-                                          sin(angle*5) * (0.3+angle.sqrt)), 
-                                     0.0));
-    entity.addComponent(new Velocity(vec2(sin(angle) * 0.5, 
-                                          cos(angle) * 0.5), 
-                                     uniform(-PI, PI)));
-    entity.addComponent(new Size(size));
-    entity.addComponent(new Mass(0.1 + size ^^ 2));
-    entity.addComponent(new Drawable(size, uniform(3, 12), uniformDistribution!float(3).vec3));
-    entity.addComponent(new Input());
+    auto entity = createEntity(world, vec2(cos(angle * 5) * (0.3 + angle.sqrt),
+                                           sin(angle * 5) * (0.3 + angle.sqrt)),
+                                      vec2(sin(angle) * 0.5, cos(angle) * 0.5),
+                                      size);
     
     entities ~= entity;
   }
   
-  playerEntity.addComponent(new Gravity(entities));
-  playerEntity.addComponent(new Collider(entities));
-  
   foreach (entity; entities)
   {
-    entity.addComponent(new Gravity(entities.filter!(checkEntity => checkEntity != entity).array ~ playerEntity));
-    entity.addComponent(new Collider(entities.filter!(checkEntity => checkEntity != entity).array ~ playerEntity));
+    entity.addComponent(new Gravity(entities.filter!(checkEntity => checkEntity != entity).array));
+    entity.addComponent(new Collider(entities.filter!(checkEntity => checkEntity != entity).array));
     entity.addToWorld();
   }
 
@@ -105,14 +86,12 @@ void main()
       renderer.zoom -= renderer.zoom * 1.0/60.0;
     
     assert(playerEntity.getComponent!Input);
-    
     playerEntity.getComponent!Input.accelerate = input.accelerate;
     playerEntity.getComponent!Input.decelerate = input.decelerate;
     playerEntity.getComponent!Input.rotateLeft = input.rotateLeft;
     playerEntity.getComponent!Input.rotateRight = input.rotateRight;
     
     renderer.draw();
-
     SDL_GL_SwapWindow(window);
   }
 }
