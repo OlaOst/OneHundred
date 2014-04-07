db6294665675c87a06075ad12453f43e1dd3b77e
OlaOst <olaa81@gmail.com>
2014-04-07 21:27:08 +0200

    Cleanup physics to 100 line limit

--- source/systems/physics.d
+++ source/systems/physics.d
@@ -18,38 +18,11 @@ import timer;
 
 class Physics : System
 {
-  invariant()
-  {
-    //assert(positions.length == masses.length);
-    
-    // ensure there is a one-to-one mapping for indices in the arrays and the indexForEntity mapping
-    /*foreach (const Entity entity, int index; indexForEntity)
-    {
-      assert(index >= 0 && index <= positions.length);
-    }*/
-  }
-  
   State[] previousStates;
   State[] currentStates;
-  
-  //vec2[] positions;
-  //float[] masses;
-  
-  Timer timer;
-  
-  this()
-  {
-  }
 
-  /*override void process(Entity entity)
-  {
-    if (entity.getComponent!Mass !is null)
-    {
-      auto state = State(entity, &calculateForce, &calculateTorque);
-      currentStates ~= state;
-    }
-  }*/
-  
+  Timer timer;
+
   override bool canAddEntity(Entity entity)
   {
     return "position" in entity.vectors && "mass" in entity.scalars;
@@ -63,41 +36,8 @@ class Physics : System
       entityForIndex[currentStates.length] = entity;
       
       auto state = State(entity, &calculateForce, &calculateTorque);
-      /*state.position = entity.vectors["position"];
-      state.mass = entity.scalars["mass"];
-      
-      if ("velocity" in entity.vectors)
-      {
-        state.velocity = entity.vectors["velocity"];
-        state.momentum = state.velocity * state.mass;
-      }*/
-      
-      //if ("angle" in entity.scalars)
-        //state.angle = entity.scalars["angle"];
-      
-      //state.forceCalculator = &calculateForce;
-      //state.torqueCalculator = &calculateTorque;
 
       currentStates ~= state;
-      
-      /+ stuff in State
-      // primaries
-      vec2 position;
-      vec2 momentum;
-      double angle;
-      //double rotationalMomentum;
-      
-      // secondaries
-      vec2 velocity;
-      double rotation;
-      
-      //constants
-      double mass;
-      
-      vec2 function(State, double time) forceCalculator;
-      double function(State, double time) torqueCalculator;
-      Entity entity;
-      +/
     }
   }
   
@@ -108,9 +48,6 @@ class Physics : System
   
   override void update()
   {
-    //import std.stdio;
-    //writeln("physics update, positions are ", currentStates.map!(state => state.position).array);
-    
     while (timer.accumulator >= timer.physicsTimeStep)
     {
       integrateStates(currentStates, previousStates, timer.time, timer.physicsTimeStep);
@@ -119,7 +56,6 @@ class Physics : System
     }
     
     interpolateStates(currentStates, previousStates, timer.accumulator / timer.physicsTimeStep);
-    //currentStates.length = 0;
   }
   
   void updateEntities()
