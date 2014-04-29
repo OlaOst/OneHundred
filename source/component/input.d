eed89a5da21835b1eb10dfdcd63fc8981f6e766d
OlaOst <olaa81@gmail.com>
2014-04-30 00:58:07 +0200

    Cleanup component\input.d

--- source/component/input.d
+++ source/component/input.d
@@ -68,50 +68,6 @@ class Input
     gameControls["removeEntity"] = SDLK_DELETE;
     gameControls["toggleDebugInfo"] = SDLK_F1;
     
-    //for (char letter = 'a'; letter <= 'z'; letter++)
-      //textInput["" ~ letter] = letter;
-    //for (char letter = '0'; letter <= '9'; letter++)
-      //textInput[letter.to!string] = letter;
     textInput["backspace"] = SDLK_BACKSPACE;
-    //textInput[" "] = SDLK_SPACE;
-    //textInput["!"] = SDLK_EXCLAIM;
-    //textInput["\""] = SDLK_QUOTEDBL;
-    
-    
-    /+
-    SDLK_UNKNOWN = 0,
-    SDLK_RETURN = '\r',
-    SDLK_ESCAPE = '\033',
-    SDLK_TAB = '\t',
-    SDLK_EXCLAIM = '!',
-    SDLK_QUOTEDBL = '"',
-    SDLK_HASH = '#',
-    SDLK_PERCENT = '%',
-    SDLK_DOLLAR = '$',
-    SDLK_AMPERSAND = '&',
-    SDLK_QUOTE = '\'',
-    SDLK_LEFTPAREN = '(',
-    SDLK_RIGHTPAREN = ')',
-    SDLK_ASTERISK = '*',
-    SDLK_PLUS = '+',
-    SDLK_COMMA = ',',
-    SDLK_MINUS = '-',
-    SDLK_PERIOD = '.',
-    SDLK_SLASH = '/',
-    SDLK_COLON = ':',
-    SDLK_SEMICOLON = ';',
-    SDLK_LESS = '<',
-    SDLK_EQUALS = '=',
-    SDLK_GREATER = '>',
-    SDLK_QUESTION = '?',
-    SDLK_AT = '@',
-
-    SDLK_LEFTBRACKET = '[',
-    SDLK_BACKSLASH = '\\',
-    SDLK_RIGHTBRACKET = ']',
-    SDLK_CARET = '^',
-    SDLK_UNDERSCORE = '_',
-    SDLK_BACKQUOTE = '`',
-    +/
   }
 }
