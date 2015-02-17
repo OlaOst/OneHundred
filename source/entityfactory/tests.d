eb0d95c03459310c1fd91f2489764a3b61b9fbc6
OlaOst <olaa81@gmail.com>
2015-02-18 00:06:37 +0100

    Add textcover for debugentity texts

--- source/entityfactory/tests.d
+++ source/entityfactory/tests.d
@@ -12,6 +12,7 @@ import components.drawables.polygon;
 import components.drawables.text;
 import components.sound;
 import entity;
+import entityfactory.texts;
 import systemset;
 
 
@@ -70,6 +71,7 @@ void addDebugEntities(SystemSet systemSet)
     text["color"] = vec4(1.0, 1.0, 1.0, 1.0);
     text["size"] = 0.1;
     systemSet.addEntity(text);
+    systemSet.addEntity(text.createTextCover(systemSet.textGraphics.getComponent(text).aabb));
     
     auto debugEntity = new Entity();
     debugEntity["position"] = position;
