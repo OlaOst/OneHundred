cd98dd55e9c166fbf2d0a5700ca6e49696bb1579
OlaOst <olaa81@gmail.com>
2015-02-18 22:32:14 +0100

    Cleanup entitfactory/texts.d

--- source/entityfactory/texts.d
+++ source/entityfactory/texts.d
@@ -7,6 +7,7 @@ import gl3n.aabb;
 import gl3n.linalg;
 
 import components.collider;
+import components.drawables.polygon;
 import entity;
 
 
@@ -31,26 +32,18 @@ Entity createTextCover(Entity textEntity, AABB textAABB)
   auto textCover = new Entity();
   textCover["position"] = textEntity.get!vec2("position");
   textCover["angle"] = 0.0.to!string;
-  /*textCover["polygon.vertices"] = [[textAABB.min.x, textAABB.min.y], 
-                                   [textAABB.min.x, textAABB.max.y], 
-                                   [textAABB.max.x, textAABB.min.y],
-                                   [textAABB.min.x, textAABB.max.y], 
-                                   [textAABB.max.x, textAABB.max.y], 
-                                   [textAABB.max.x, textAABB.min.y]];
-  textCover["polygon.colors"] = [0.0, 0.5, 0.5, 0.5].repeat.take(6).array;*/
-  import components.drawables.polygon;
+
   textCover.polygon = new Polygon([vec2(textAABB.min.x, textAABB.min.y), 
                                    vec2(textAABB.min.x, textAABB.max.y), 
                                    vec2(textAABB.max.x, textAABB.min.y),
                                    vec2(textAABB.min.x, textAABB.max.y), 
                                    vec2(textAABB.max.x, textAABB.max.y), 
                                    vec2(textAABB.max.x, textAABB.min.y)],
-                                   vec4(0.0, 0.5, 0.5, 0.5).repeat.take(6).array);
+                                   vec4(0.0, 0.5, 0.5, 0.85).repeat.take(6).array);
   
   textCover["relation.types"] = ["RelativeValues", "SameShape", "DieTogether"];
   textCover["relation.targetId"] = textEntity.id;
   textCover["relation.value.position"] = vec2(0.0, 0.0);
-  //textCover["relation.value.aabb"] = "same";//AABB(vec3(0,0,0),vec3(0,0,0));
   
   return textCover;
 }
