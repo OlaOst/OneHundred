5edb1e11e0535ac650cfdee1d3e6d386da4e8085
OlaOst <olaa81@gmail.com>
2015-02-19 21:47:13 +0100

    Cleanup timer.d

--- source/timer.d
+++ source/timer.d
@@ -19,14 +19,10 @@ public:
   void incrementAccumulator() //@nogc
   {
     double newTime = timer.peek().usecs * (1.0 / 1_000_000);
-    //import core.time;
-    //double newTime = (TickDuration.currSystemTick - TickDuration.appOrigin).length / cast(double)TickDuration.ticksPerSec;
     frameTime = min(newTime - currentTime, maxFrametime);
     
     currentTime = newTime;
     accumulator += frameTime;
-    //import std.stdio;
-    //writeln("fps: ", 1.0/frameTime);
   }
 
   private StopWatch timer;
@@ -34,8 +30,7 @@ public:
   
   public double frameTime = 0.0;
   
-  //public double time = 0.0;
-  public immutable double maxFrametime;// = 0.25;
-  public immutable double timeStep;// = 1.0 / 60.0;
+  public immutable double maxFrametime;
+  public immutable double timeStep;
   public double accumulator = 0.0;
 }
