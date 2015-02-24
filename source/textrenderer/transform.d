691969cb400746b999730eea04069480da4f713e
OlaOst <olaa81@gmail.com>
2015-02-24 23:56:07 +0100

    Refactor textrenderer/transform.d to 100 line limit

--- source/textrenderer/transform.d
+++ source/textrenderer/transform.d
@@ -11,61 +11,42 @@ import components.drawables.text;
 import textrenderer.textrenderer;
 
 
-vec2[] getVerticesForText(TextRenderer textRenderer, 
-                          //Text text, double zoom, vec2 delegate (vec2) transform)
-                          //Text text, double zoom, vec2 function (vec2, Text, Camera) @nogc transform) @nogc
-                          Text text, Camera camera) @nogc
+vec2[] getVerticesForText(TextRenderer textRenderer, Text text, Camera camera) @nogc
 {
   assert(camera !is null);
   
-  //vec2[] result;
   vec2[65536] buffer;
   size_t index = 0;
   
   auto cursor = vec2(0.0, 0.0);
 
-  //auto lines = text.text.split("\n");
   auto lines = text.text.splitter("\n");
   foreach (line; lines)
   {
     foreach (letter; line)
     {
       auto glyph = textRenderer.getGlyphForLetter(letter);
-      //auto textTransform = delegate (vec2 vertex) => transform(vertex) + 
-                                                     //(glyph.offset * text.size + cursor) * zoom;
-
-      //result ~= text.vertices.map!textTransform.array();
-      //auto textTransformedVertices = text.vertices.map!textTransform;
-      //auto textTransformedVertices = text.vertices.map!(vertex => transform(vertex, text, null) + (glyph.offset * text.size + cursor) * zoom);
       
       foreach (vertex; text.vertices)
       {
-        //vec3(vertex, 0.0)*mat3.zrotation(-component.angle)).xy + component.position - camera.position) * camera.zoom;
-        buffer[index] = ((vec3(vertex, 0.0) * mat3.zrotation(-text.angle)).xy + text.position - camera.position) * camera.zoom + (glyph.offset * text.size + cursor) * camera.zoom;
-      
-        //buffer[index] = transform(vertex, text, null) + (glyph.offset * text.size + cursor) * zoom;
+        buffer[index] = ((vec3(vertex, 0.0) * mat3.zrotation(-text.angle)).xy + 
+                         text.position - camera.position) * 
+                        camera.zoom + (glyph.offset * text.size + cursor) * camera.zoom;
         index++;
       }
-      
-      //foreach (textTransformedVertex; textTransformedVertices)
-        //result ~= textTransformedVertex;
       
       cursor += glyph.advance * text.size;
     }
     cursor = vec2(0.0, cursor.y - text.size * 2.0);
   }
   
-  //return result;
   return buffer[0..index];
 }
 
 vec2[] getTexCoordsForText(TextRenderer textRenderer, Text text) @nogc
 {
-  //auto lines = text.text.split("\n");
   auto lines = text.text.splitter("\n");
   
-  //import std.algorithm;
-  //auto letterCount = reduce!((count, line) => count + line.length)(0, lines);
   auto letterCount = lines.map!(line => line.count).sum;
   static vec2[65536] buffer;
   
@@ -76,7 +57,6 @@ vec2[] getTexCoordsForText(TextRenderer textRenderer, Text text) @nogc
   {
     foreach (letter; line)
     {
-      //result ~= textRenderer.getTexCoordsForLetter(letter);
       buffer[index*6 .. (index+1)*6] = textRenderer.getTexCoordsForLetter(letter);
       index++;
     }
