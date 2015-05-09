a9828370a7cfb8e4fab7384d5cc2dfc21092f2f9
OlaOst <olaa81@gmail.com>
2015-05-09 10:35:00 +0200

    Quick render fix for MacOS

--- source/renderer/coloredtexturerenderer.d
+++ source/renderer/coloredtexturerenderer.d
@@ -6,27 +6,29 @@ import glamour.vbo;
 import gl3n.linalg;
 
 
-public void drawColoredTexture(Shader shader, mat4 transform, 
+public void drawColoredTexture(Shader shader, mat4 transform,
                                vec3[] vertices, vec2[] texCoords, vec4[] colors)
 {
   assert(vertices.length == texCoords.length);
   assert(vertices.length == colors.length);
-  
+
   auto verticesBuffer = new Buffer(vertices);
   auto textureBuffer = new Buffer(texCoords);
   auto colorsBuffer = new Buffer(colors);
-  
+
   shader.bind();
-  
+
   shader.uniform("transform", transform);
-  
+
   verticesBuffer.bind(shader, "position", GL_FLOAT, 3, 0, 0);
   textureBuffer.bind(shader, "texCoords", GL_FLOAT, 2, 0, 0);
   colorsBuffer.bind(shader, "color", GL_FLOAT, 4, 0, 0);
-  
+
   checkgl!glDrawArrays(GL_TRIANGLES, 0, cast(int)(vertices.length));
-  
-  verticesBuffer.remove();
+
+  version(OSX) {} else
+    verticesBuffer.remove();
+
   textureBuffer.remove();
   colorsBuffer.remove();
 }
--- source/renderer/polygonrenderer.d
+++ source/renderer/polygonrenderer.d
@@ -8,19 +8,19 @@ import gl3n.linalg;
 public void drawPolygons(Shader shader, mat4 transform, vec3[] vertices, vec4[] colors)
 {
   assert(vertices.length == colors.length);
-  
+
   auto verticesBuffer = new Buffer(vertices);
   auto colorsBuffer = new Buffer(colors);
-  
+
   shader.bind();
-  
+
   shader.uniform("transform", transform);
-  
+
   verticesBuffer.bind(shader, "position", GL_FLOAT, 3, 0, 0);
   colorsBuffer.bind(shader, "color", GL_FLOAT, 4, 0, 0);
-  
+
   checkgl!glDrawArrays(GL_TRIANGLES, 0, cast(int)(vertices.length));
-  
+
   verticesBuffer.remove();
   colorsBuffer.remove();
 }
