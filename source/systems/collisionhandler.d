f23fbd94214553457a53369cc87c1434880ab1f5
OlaOst <olaa81@gmail.com>
2015-09-05 10:31:16 +0200

    Refactor collisionhandler to 100 line limit

--- source/systems/collisionhandler.d
+++ source/systems/collisionhandler.d
@@ -1,17 +1,14 @@
 module systems.collisionhandler;
 
 import std.algorithm;
-import std.conv;
 import std.datetime;
 import std.range;
 import std.regex;
-import std.stdio;
 
 import gl3n.linalg;
 
 import collision.responsehandler;
 import components.collider;
-import converters;
 import entity;
 import spatialindex.spatialindex;
 import system;
@@ -19,7 +16,6 @@ import system;
 
 class CollisionHandler : System!Collider
 {
-  SpatialIndex!Collider index = new SpatialIndex!Collider();
   Entity[] collisionEffectParticles;
 
   bool canAddEntity(Entity entity)
@@ -35,53 +31,52 @@ class CollisionHandler : System!Collider
     if (entity.has("collisionfilter"))
     {
       auto collisionFilter = regex(entity.get!string("collisionfilter"));
-      component.colliderIdsToIgnore = entityForIndex.byValue.filter!(checkEntity => checkEntity.get!string("fullName").matchFirst(collisionFilter))
-                                                            .map!(checkEntity => checkEntity.id).array.dup;
+      component.colliderIdsToIgnore = 
+        entityForIndex.byValue.filter!(checkEntity => checkEntity.get!string("fullName")
+                                                                 .matchFirst(collisionFilter))
+                              .map!(checkEntity => checkEntity.id).array.dup;
     }
     
-    entityForIndex.byValue.filter!(checkEntity => checkEntity.has("collisionfilter"))
-                          .filter!(checkEntity => entity.get!string("fullName").matchFirst(checkEntity.get!string("collisionfilter")))
-                          .each!(checkEntity => getComponent(checkEntity).colliderIdsToIgnore ~= entity.id);
+    entityForIndex.byValue
+      .filter!(checkEntity => checkEntity.has("collisionfilter"))
+      .filter!(checkEntity => entity.get!string("fullName")
+                                    .matchFirst(checkEntity.get!string("collisionfilter")))
+      .each!(checkEntity => getComponent(checkEntity).colliderIdsToIgnore ~= entity.id);
     
     component.updateFromEntity(entity);
     return component;
   }
-
+  
   void updateValues()
   {
-    int broadPhaseCount, narrowPhaseCount;
-    
+    auto index = new SpatialIndex!Collider();
+
     auto broadPhaseTimer = StopWatch(AutoStart.yes);
-    components.each!(collisionEntity => index.insert(collisionEntity));
+    components.each!(component => index.insert(component));
     auto candidates = index.overlappingElements();
     broadPhaseTimer.stop();
-    broadPhaseCount += candidates.length;
 
     auto narrowPhaseTimer = StopWatch(AutoStart.yes);
     auto collisions = cartesianProduct(candidates, candidates)
                 .filter!(candidatePair => filterCandidates(candidatePair[0], candidatePair[1]))
                 .map!(collisionPair => Collision(collisionPair[0], collisionPair[1])).array;
     narrowPhaseTimer.stop();
-    narrowPhaseCount += collisions.length;
 
     components.each!(component => component.overlappingColliders.length = 0);
 
     foreach (collision; collisions)
     {
-      collision.first.isColliding = true;
-      collision.other.isColliding = true;
+      collision.first.isColliding = collision.other.isColliding = true;
       collision.first.overlappingColliders ~= collision.other;
       collision.other.overlappingColliders ~= collision.first;
     }
 
     debugText = format("collisionhandler checked %s/%s candidates\nbroadphase/narrowphase",
-                       broadPhaseCount, narrowPhaseCount) ~
+                       candidates.length, collisions.length) ~
                 format("\ncollisionhandler timings %s/%s milliseconds\nbroadphase/narrowphase",
                        broadPhaseTimer.peek.usecs*0.001, narrowPhaseTimer.peek.usecs*0.001);
 
     collisionEffectParticles ~= collisions.handleCollisions(this);
-
-    index = new SpatialIndex!Collider();
   }
 
   bool filterCandidates(Collider left, Collider right)
