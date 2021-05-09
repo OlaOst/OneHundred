d4c418dd1b85c59cc9e463d2b675c0e092ba9a7e
OlaOst <olaa81@gmail.com>
2021-05-09 16:48:52 +0200

    Replace glamour library with built-in source, modified to work with bindbc

---
+++ source/glamour/gl.d
@@ -0,0 +1,21 @@
+/**
+If compiled with version=Derelict3 glamour will use Derelict3 as OpenGL backend, otherwise Derelict2.
+If glamour gets compiled with Derelict3, you can not load textures from images with DevIL!
+*/
+
+
+module glamour.gl;
+
+public import bindbc.opengl;
+public import bindbc.opengl.gl;
+
+/+version(glad) {
+    public import glad.gl.gl;
+} else version(Derelict3) {
+    public import derelict.opengl3.gl3;
+} else version(Derelict2) {
+    public import derelict.opengl.gl;
+    public import derelict.opengl.glext;
+} else {
+    static assert(false, "Requires at least \"glad\", \"Derelict2\" or \"Derelict3\", via -version");   
+}+/
